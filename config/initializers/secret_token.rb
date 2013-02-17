0d7754ebb7f27514ce83e3184904714fdcfcb1d2
Josh Hubbard <jhubbardsf@gmail.com>
2013-02-16 23:10:53 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Missingpiece::Application.config.secret_token = 'c80d0e1ce0da17d0c24a8c0e5059b9dd1d7c0010e138308701b15ec527c497c7296370aef124c23e5dfe09d4b2adfc7e8f472eb1e33e5bc7df80d9f361dac76f'
