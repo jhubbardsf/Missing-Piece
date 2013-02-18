35c59fac5b6910c1cb9e102390566401b273f3bb
Josh Hubbard <jhubbardsf@gmail.com>
2013-02-18 12:27:28 -0800

    Added in footer, fixed header/footer, and some css.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.11'
 gem 'haml-rails', '0.3.5'
 gem 'jquery-rails', '2.2.0'
-gem 'bootstrap-sass', '2.1.0'
+gem 'bootstrap-sass', '2.3.0'
 gem 'sqlite3'
 
 group :assets do
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,18 +1,29 @@
 var boxes = [];
 
 $(function () {
+    // Throw all boxes into array of box objects
     $('.box').each(function () {
         var newBox = box($(this));
         boxes.push(newBox);
         $(this).data('box', newBox);
     });
 
+    // Move the box clicked if it has a move
     $(document).on('click', '.box', function (e) {
         var box = $(this).data('box');
         box.findMove();
     });
 
+    // Toggle number overlay
+    $(document).on('click', '#numbers-toggle', function(e) {
+        e.preventDefault();
+       $('.text-holder').toggle();
+    });
+
+
+    // Mess the boxes up
     $(document).on('click', '#randomize', function (e) {
+        e.preventDefault();
         var possibleBoxes = [],
             boxToMove = {};
 
