a1ebe070ff493b0bc5293e90c070c1fa0eff7cbe
Josh Hubbard <jhubbardsf@gmail.com>
2013-02-17 19:29:15 -0800

    Got the basic thing working.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,67 +1,182 @@
 $(function () {
-    var hasEmptySpace = function($box) {
-        console.log("text is " + $box.find('.text-holder').text());
-        return($box.find('.text-holder').is(':empty'));
-    };
-
-    var moveIfEmpty = function($boxOriginal, $boxFuture) {
-        console.log("moveIfEmpty contents");
-        console.log("move " + $boxOriginal.text() + " to " + $boxFuture);
-        console.log($($boxFuture).find('.text-holder'));
-        if (hasEmptySpace($boxFuture)) {
-             console.log("SHOULD MOVE");
-            $boxFuture.find('.text-holder').append($boxOriginal.find('.text-holder').text());
-            $boxOriginal.find('.text-holder').empty();
+    var findAvailable = function () {
+        var movablePieces = [];
+        for (var row = 0; row < 4; row++) {
+            for (var col = 0; col < 4; col++) {
+                if ($("[data-col=" + col + "][data-row=" + row + "]").length > 0) {
+                    // THE BOX EXISTS
+                    $box = $("[data-col=" + col + "][data-row=" + row + "]")[0];
+                    if (canMove("[data-col=" + col + "][data-row=" + row + "]").length > 0) {
+                        movablePieces.push($box);
+                    }
+                }
+            }
         }
-    };
 
-    $(document).on('click', '.col', function(e) {
-        var $boxInQuestion = false,
-            $this          = $(this),
-            thisRow        = $this.data('row'),
-            thisCol        = $this.data('col');
+        return movablePieces;
+    }
 
-        if (thisRow > 1) {
+    var canMove = function (identifier) {
+        var possibleMoves = [];
+        if ($(identifier).attr('data-row') > 0) {
             // Check above
-            $boxInQuestion =  $('.row-' + (thisRow - 1) + '.col-' + thisCol);
-            console.log("above being empty is " + hasEmptySpace($boxInQuestion));
-            moveIfEmpty($this, $boxInQuestion);
+            var nextRow = (parseInt($(identifier).attr('data-row')) - 1),
+                nextCol = $(identifier).attr('data-col');
+            if (!$("[data-col=" + nextCol + "][data-row=" + nextRow + "]").length > 0) {
+                possibleMoves.push(["top", "-=100px"]);
+            }
+
         }
 
-        if (thisRow < 4) {
+        if ($(identifier).attr('data-row') < 3) {
             // Check below
-            $boxInQuestion =  $('.row-' + (thisRow + 1) + '.col-' + thisCol);
-            console.log("below being empty is " + hasEmptySpace($boxInQuestion));
-            moveIfEmpty($this, $boxInQuestion);
+            var nextRow = (parseInt($(identifier).attr('data-row')) + 1),
+                nextCol = $(identifier).attr('data-col');
+            if (!$("[data-col=" + nextCol + "][data-row=" + nextRow + "]").length > 0) {
+                possibleMoves.push(["top", "+=100px"]);
+            }
+
         }
 
-        if (thisCol > 1) {
-            // Check to the left
-            $boxInQuestion =  $('.row-' + thisRow + '.col-' + (thisCol - 1));
-            console.log("left being empty is " + hasEmptySpace($boxInQuestion));
-            moveIfEmpty($this, $boxInQuestion);
+        if ($(identifier).attr('data-col') > 0) {
+            // Check left
+            var nextRow = $(identifier).attr('data-row'),
+                nextCol = (parseInt($(identifier).attr('data-col')) - 1);
+            if (!$("[data-col=" + nextCol + "][data-row=" + nextRow + "]").length > 0) {
+                possibleMoves.push(["left", "-=100px"]);
+            }
+
         }
 
-        if (thisCol < 4) {
-            // Check to the right
-            $boxInQuestion =  $('.row-' + thisRow + '.col-' + (thisCol + 1));
-            console.log("right being empty is " + hasEmptySpace($boxInQuestion));
-            moveIfEmpty($this, $boxInQuestion);
+        if ($(identifier).attr('data-col') < 3) {
+            // Check right
+            var nextRow = $(identifier).attr('data-row'),
+                nextCol = (parseInt($(identifier).attr('data-col')) + 1);
+            if (!$("[data-col=" + nextCol + "][data-row=" + nextRow + "]").length > 0) {
+                possibleMoves.push(["left", "+=100px"]);
+            }
+        }
+
+        return possibleMoves;
+    }
+
+    var lastClicked = $('.box')[0];
+    var randomizeBoard = function () {
+
+        for (var i = 0; i < 60; i++) {
+            var movable = findAvailable();
+            movable = shuffleArray(movable);
+            if (lastClicked == movable[0]) {
+                $(movable[1]).click();
+                lastClicked = movable[1];
+            } else {
+                $(movable[0]).click();
+                lastClicked = movable[0];
+            }
+        }
+    };
+
+    $(document).on('click', '.box', function (e) {
+        var changed = false;
+
+        if ($(this).attr('data-row') > 0 && !changed) {
+            // Check above
+            var nextRow = (parseInt($(this).attr('data-row')) - 1),
+                nextCol = $(this).attr('data-col');
+            if (!$("[data-col=" + nextCol + "][data-row=" + nextRow + "]").length > 0) {
+                $(this).animate({ "top": '-=100px' }, 1000, function () {
+                    checkIfWin();
+                })
+                changed = true;
+            }
+
+        }
+
+        if ($(this).attr('data-row') < 3 && !changed) {
+            // Check below
+            var nextRow = (parseInt($(this).attr('data-row')) + 1),
+                nextCol = $(this).attr('data-col');
+            if (!$("[data-col=" + nextCol + "][data-row=" + nextRow + "]").length > 0) {
+                $(this).animate({ "top": '+=100px' }, 1000, function () {
+                    checkIfWin();
+                }) // Move box down
+                changed = true;
+            }
+
+        }
+
+        if ($(this).attr('data-col') > 0 && !changed) {
+            // Check left
+            var nextRow = $(this).attr('data-row'),
+                nextCol = (parseInt($(this).attr('data-col')) - 1);
+            if (!$("[data-col=" + nextCol + "][data-row=" + nextRow + "]").length > 0) {
+                console.log("Left does not exist.");
+                $(this).animate({ "left": '-=100px' }, 1000, function () {
+                    checkIfWin();
+                }) // Move box down
+                changed = true;
+            }
+
+        }
+
+        if ($(this).attr('data-col') < 3 && !changed) {
+            // Check right
+            var nextRow = $(this).attr('data-row'),
+                nextCol = (parseInt($(this).attr('data-col')) + 1);
+            if (!$("[data-col=" + nextCol + "][data-row=" + nextRow + "]").length > 0) {
+                $(this).animate({ "left": '+=100px' }, 1000, function () {
+                    checkIfWin();
+                }) // Move box down
+                changed = true;
+            }
+        }
+
+        if (changed) {
+            $(this).attr('data-row', nextRow);
+            $(this).attr('data-col', nextCol);
+            ;
         }
     });
 
+    var checkIfWin = function () {
+        var won = true;
+        for (var row = 0; row < 4; row++) {
+            for (var col = 0; col < 4; col++) {
+                var possible = $("[data-col=" + row + "][data-row=" + col + "]");
+                if (possible.length > 0) {
+                    var correct = parseInt(possible.data('correct')),
+                        calculated = parseInt(possible.attr('data-row')) * 4 + parseInt(possible.attr('data-col'));
 
-    for (var row = 1; row <= 4; row++) {
-        for (var col = 1; col <= 4; col++) {
-            if ((row !== 4) || (col !== 4)) {
-                var rowClass         = ".row-" + row,
-                    colClass         = ".col-" + col,
-                    rowColTextHolder = rowClass + " " + colClass + " > .text-holder";
-                $(rowColTextHolder).html(row + " " + col);
+                    if (correct != calculated) {
+                        won = false;
+                    }
+
+                }
             }
         }
-    }
+
+        if (won) {
+            alert("We won!");
+        }
+    };
+
+    $(document).on('click', '#randomize', function(e) {
+        e.preventDefault();
+        randomizeBoard();
+    });
 });
 
-// Functions
 
+/**
+ * Randomize array element order in-place.
+ * Using Fisher-Yates shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
